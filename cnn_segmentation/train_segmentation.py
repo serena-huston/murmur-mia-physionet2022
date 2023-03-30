49d5fb451f85d56fbf57b7f6b14aee202f7f32e6
serena-huston <serena_huston@yahoo.co.uk>
2023-03-30 12:02:06 +0100

    Changing back

--- cnn_segmentation/train_segmentation.py
+++ cnn_segmentation/train_segmentation.py
@@ -53,7 +53,7 @@ def set_up_model():
     criterion = nn.CrossEntropyLoss()
 
 
-def fit_model(trainloader, epochs=7):
+def fit_model(trainloader, epochs=6):
     model.train(True)
 
     for epoch in range(epochs):
